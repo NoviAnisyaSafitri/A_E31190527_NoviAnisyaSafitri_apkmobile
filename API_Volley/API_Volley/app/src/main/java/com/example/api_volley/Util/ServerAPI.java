624426141de02f04770ecf026fa79ce25e6cafb8
NoviAnisyaSafitri <syasaanggraini@gmail.com>
2021-06-09 00:10:18 +0700

    API_Volley

---
+++ API_Volley/API_Volley/app/src/main/java/com/example/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.example.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.119/A_E31190279_AJI-SAMPURNO_WEB/MINGGU_12/CI/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.43.119/A_E31190279_AJI-SAMPURNO_WEB/MINGGU_12/CI/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.43.119/A_E31190279_AJI-SAMPURNO_WEB/MINGGU_12/CI/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.43.119/A_E31190279_AJI-SAMPURNO_WEB/MINGGU_12/CI/Mahasiswa/ApiUpdate";
+}
